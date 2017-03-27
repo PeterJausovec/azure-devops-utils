e0cf291c2b730963f060e62b21dc6e94f6bd2aba
Peter Jausovec <peterj@microsoft.com>
2017-03-27 11:25:36 -0700

    typo

--- jenkins/add-cd-job.sh
+++ jenkins/add-cd-job.sh
@@ -12,5 +12,5 @@ cd_job_xml=$(curl -s ${artifacts_location}/jenkins/cd-job.xml)
 cd_job_xml=${cd_job_xml//'{insert-ci-job-here}'/${ci_job_name}}
 cd_job_xml=${cd_job_xml//'{insert-git-url-here}'/${git_url}}
 
-echo "${job_xml}" > cdjob.xml
+echo "${cd_job_xml}" > cdjob.xml
 cat cdjob.xml | java -jar jenkins-cli.jar -s ${jenkins_url} create-job ${cd_job_name} 
